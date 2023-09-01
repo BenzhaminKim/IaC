289192e1198243f8956e4478700fc77010508a68
BenzhaminKim <dudgnl37@gmail.com>
2023-09-01 09:27:10 +0900

    Add terraform practices

--- learn-terraform-aws-ec2/main.tf
+++ learn-terraform-aws-ec2/main.tf
@@ -13,6 +13,109 @@ provider "aws" {
   region = "us-east-1"
 }
 
+# Create a VPC
+resource "aws_vpc" "vpc1" {
+  cidr_block = "10.0.0.0/16"
+}
+
+# Create Internet Gateway
+resource "aws_internet_gateway" "gw1" {
+  vpc_id = aws_vpc.vpc1.id
+
+  tags = {
+    Name = "main"
+  }
+}
+
+# Create Route Table
+resource "aws_route_table" "rt1" {
+  vpc_id = aws_vpc.vpc1.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.gw1.id
+  }
+
+  route {
+    ipv6_cidr_block = "::/0"
+    egress_only_gateway_id = aws_internet_gateway.gw1.id
+  }
+
+  tags = {
+    Name = "example"
+  }
+}
+
+# Create Subnet
+resource "aws_subnet" "sb1" {
+  vpc_id     = aws_vpc.vpc1.id
+  cidr_block = "10.0.1.0/24"
+  availability_zon = "us-east-1a"
+
+  tags = {
+    Name = "Main"
+  }
+}
+
+resource "aws_route_table_association" "dev-route-association" {
+  subnet_id      = aws_subnet.sb1.id
+  route_table_id = aws_route_table.rt1.id
+}
+
+resource "aws_security_group" "allow_web" {
+  name        = "allow_web"
+  description = "Allow Web inbound traffic"
+  vpc_id      = aws_vpc.vpc1.id
+
+  ingress {
+    description      = "HTTPS from VPC"
+    from_port        = 443
+    to_port          = 443
+    protocol         = "tcp"
+    cidr_blocks      = [0.0.0.0/0]
+  }
+
+  ingress {
+    description      = "HTTP from VPC"
+    from_port        = 80
+    to_port          = 80
+    protocol         = "tcp"
+    cidr_blocks      = [0.0.0.0/0]
+  }
+
+  ingress {
+    description      = "SSH from VPC"
+    from_port        = 22
+    to_port          = 22
+    protocol         = "tcp"
+    cidr_blocks      = [0.0.0.0/0]
+  }
+  egress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "allow_web"
+  }
+}
+
+resource "aws_network_interface" "dev-nic" {
+  subnet_id       = aws_subnet.sb1.id
+  private_ips     = ["10.0.0.0"]
+  security_groups = [aws_security_group.allow_web.id]
+
+}
+
+resource "aws_eip" "one" {
+  vpc                    = true
+  network_interface         = aws_network_interface.dev-nic.id
+  associate_with_private_ip = "10.0.0.0"
+  depends_on = aws_internet_gateway.gw1
+}
+
 resource "aws_instance" "app_server" {
   ami           = "ami-0574da719dca65348"
   instance_type = "t2.micro"
